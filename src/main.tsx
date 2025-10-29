ea8ebd4e23906e04b494a33a895a936f75be3d7e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-29 23:06:25 +0300

    fix: main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-// import './index.css';
+
+import '@gravity-ui/uikit/styles/fonts.css';
+import '@gravity-ui/uikit/styles/styles.css';
+
 import App from './App';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
