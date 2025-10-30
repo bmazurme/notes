6cc6841cd1442960f21213609f288cedf0f77fe0
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-10-30 10:22:22 +0300

    fix: app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
+import React, { useState } from 'react';
 import { ThemeProvider, Toaster, ToasterComponent, ToasterProvider } from '@gravity-ui/uikit';
-import type { MarkupString } from '@gravity-ui/markdown-editor';
-
+import { type MarkupString } from '@gravity-ui/markdown-editor';
+import Note from './components/Note';
 import { Editor } from './components/Editor';
 
 const toaster = new Toaster();
 
+
 function App() {
+  const [html, setHtml] = useState('**some** `$ text`');
   const handleSubmit = (value: MarkupString) => {
-    console.log(value);
+    // console.log(value);
+    setHtml(value);
   };
 
   return (
     <ThemeProvider theme="light">
       <ToasterProvider toaster={toaster}>
         <ToasterComponent />
-          <Editor onSubmit={handleSubmit} />
+        <div className="gravity-ui-landing-layout__wrapper">
+          <div className="pc-Grid gravity-ui-landing-themes__grid">
+            <div className="container-fluid">
+              <Note value={html} />
+              <Note value={html} />
+              <Note value={html} />
+              <Editor onSubmit={handleSubmit} />
+            </div>
+          </div>
+        </div>
       </ToasterProvider>
     </ThemeProvider>
   )
